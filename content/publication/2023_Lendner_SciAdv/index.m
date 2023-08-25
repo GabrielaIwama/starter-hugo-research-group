b8a1fe4c03f9dd57bae24de78b81600bbbd6fd14
GabrielaIwama <gabriela.yukari.iwama@gmail.com>
2023-08-25 11:16:55 +0200

    Add 2023_Lendner_SciAdv on publications.

---
+++ content/publication/2023_Lendner_SciAdv/index.m
@@ -0,0 +1,67 @@
+---
+title: 'Subspace partitioning in human prefrontal cortex resolves cognitive interference'
+authors:
+  - Janna Lendner
+  - N. Niethard
+  - B. Mander
+  - Frank van Schalkwijk
+  - S. Schuh-Hofer
+  - H. Schmidt
+  - R. Knight
+  - J. Born
+  - M. Walker
+  - J. Lin
+  - Randolph Helfrich
+date: '2023-08-25T00:00:00Z'
+doi: '10.1101/2022.05.13.491801'
+
+# Schedule page publish date (NOT publication's date).
+publishDate: '2022-01-01T00:00:00Z'
+
+# Publication type.
+# Legend: 0 = Uncategorized; 1 = Conference paper; 2 = Journal article;
+# 3 = Preprint / Working Paper; 4 = Report; 5 = Book; 6 = Book section;
+# 7 = Thesis; 8 = Patent
+publication_types: ['2']
+
+# Publication name and optional abbreviated publication name.
+publication: '*Science Advances*'
+publication_short: ''
+
+abstract: The proposed mechanisms of sleep-dependent memory consolidation involve the overnight regulation of neural activity at both synaptic and whole-network levels. Now, there is a lack of in vivo data in humans elucidating if, and how, sleep and its varied stages balance neural activity, and if such recalibration benefits memory. We combined electrophysiology with in vivo two-photon calcium imaging in rodents as well as intracranial and scalp electroencephalography (EEG) in humans to reveal a key role for non-oscillatory brain activity during rapid eye movement (REM) sleep to mediate sleep-dependent recalibration of neural population dynamics. The extent of this REM sleep recalibration predicted the success of overnight memory consolidation, expressly the modulation of hippocampal—neocortical activity, favoring remembering rather than forgetting. The findings describe a non-oscillatory mechanism how human REM sleep modulates neural population activity to enhance long-term memory.
+tags:
+featured: false
+
+# links:
+# - name: ""
+#   url: ""
+url_pdf: 'uploads/papers/2023_Lendner_SciAdv.pdf'
+url_code: ''
+url_dataset: ''
+url_poster: ''
+url_project: ''
+url_slides: ''
+url_source: ''
+url_video: ''
+
+# Featured image
+# To use, add an image named `featured.jpg/png` to your page's folder.
+image:
+  caption: ''
+  focal_point: ''
+  preview_only: false
+
+# Associated Projects (optional).
+#   Associate this publication with one or more of your projects.
+#   Simply enter your project's folder or file name without extension.
+#   E.g. `internal-project` references `content/project/internal-project/index.md`.
+#   Otherwise, set `projects: []`.
+projects: []
+
+# Slides (optional).
+#   Associate this publication with Markdown slides.
+#   Simply enter your slide deck's filename without extension.
+#   E.g. `slides: "example"` references `content/slides/example/index.md`.
+#   Otherwise, set `slides: ""`.
+slides:
+---
